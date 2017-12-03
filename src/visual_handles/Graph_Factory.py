698ea1ebc0e4f9226f25a4893cc4ec2e7a804cd3
Gabriel Sammut <gabsamm@gmail.com>
2017-12-03 19:47:03 +0000

    Uploading Scatter Plot Graphs

--- src/visual_handles/Graph_Factory.py
+++ src/visual_handles/Graph_Factory.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import src.util.statistics as st
 #
 class GraphFactory():
     #
@@ -11,9 +12,13 @@ class GraphFactory():
         Points marked in blue signals eye closed, red signals eye open """
         #
         x1, x2, Y = np.asarray(f1), np.asarray(f2), np.asarray(Y)
+        x1_confidence_interval_min, x1_confidence_interval_max = st.get_confidence_interval(f1, f1.columns.values, .99)
+        x2_confidence_interval_min, x2_confidence_interval_max = st.get_confidence_interval(f2, f2.columns.values, .99)
         #
-        for i in range(10000):
-            plt.scatter(x1[i], x2[i], c='b' if Y[i] == 0 else 'r')
+        for i in range(len(Y)):
+            if x1[i] > x1_confidence_interval_min and x1[i] < x1_confidence_interval_max \
+                    and x2[i] > x2_confidence_interval_min and x2[i] < x2_confidence_interval_max:
+                plt.scatter(x1[i], x2[i], c='b' if Y[i] == 0 else 'r')
         #
         plt.title('Scatter plot ' + f1.columns.values + ' vs ' + f2.columns.values)
         plt.xlabel(f1.columns.values)
