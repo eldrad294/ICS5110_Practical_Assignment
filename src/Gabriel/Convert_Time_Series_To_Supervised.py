3189db4240d7ae82b0d8dc4f763ef1c3bd3edc5f
Gabriel Sammut <gabsamm@gmail.com>
2017-12-21 10:53:38 +0000

    Added Timeseries shifting of dataset

--- src/Gabriel/Convert_Time_Series_To_Supervised.py
+++ src/Gabriel/Convert_Time_Series_To_Supervised.py
@@ -37,13 +37,27 @@ def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
         agg.dropna(inplace=True)
     return agg
 #
+def get_outlier_indexes(df, df_count, outlier_value=5000):
+    """ Takes a dataframe and returns the position of any outliers """
+    outLierIndexes = set()
+    upperLimit = outlier_value
+    for x in range(df_count):
+        df = np.array(df)
+        outLiers = np.where(df[:, x] > upperLimit)[0]
+        if len(outLiers) > 0:
+            [(outLierIndexes.add(outLiers[i])) for i in range(len(outLiers))]
+    #
+    outLierIndexes = list(outLierIndexes)
+    outLierIndexes.sort()
+    return outLierIndexes
+#
 path = '../data/EEG/EEGEyeState.csv'
 df = pd.read_csv(path)
 df_headers = df.columns
 featureCount = 14
 #
 # N-Step Univariate Forecasting Shift
-lag = 1
+lag = 3
 shifted_df = series_to_supervised(data=df, n_in=lag, n_out=1, dropnan=True)
 shifted_df_headers = shifted_df.columns
 shifted_feature_count = len(shifted_df_headers)
@@ -82,12 +96,45 @@ cmap = sns.diverging_palette(240, 20, as_cmap=True)
 # Draw the heatmap
 sns.heatmap(corr, mask=mask, cmap=cmap, vmax=1.0, vmin=-1.0, center=0,
             square=True, linewidths=.5, cbar_kws={"shrink": .5})
-#
 plt.show()
 #
 # Store the upper triangle of the correlation matrix into an Excel sheet
 corrUpperTri = corr.where(mask)
 writer = pd.ExcelWriter('../data/EEG/EEG_Shifted_Correlation_Matrix.xlsx')
+corrUpperTri.to_excel(writer, 'CorrelationMatrix')
+writer.save()
+#
+# Plot Pearson's Correlation Matrix for shifted_df without outliers
+eegDF = pd.DataFrame(data=shifted_df_X[0:shifted_df_X_count], columns=shifted_columns[0:shifted_df_X_count])
+outLierIndexes = get_outlier_indexes(eegDF, shifted_df_X_count, 5000)
+eegDF = np.delete(eegDF, outLierIndexes, 0)
+#
+# Compute the correlation matrix
+corr = eegDF.corr()
+#
+# Generate a mask for the upper triangle
+mask = np.zeros_like(corr, dtype=np.bool)
+mask[np.triu_indices_from(mask)] = True
+#
+# Set up the matplotlib figure
+fig = plt.figure()
+fig.set_figheight(20)
+fig.set_figwidth(20)
+#
+ax = fig.add_subplot(1, 1, 1)
+ax.set_title("Feature correlation matrix excluding outliers")
+#
+# Generate a custom diverging colormap
+cmap = sns.diverging_palette(240, 20, as_cmap=True)
+#
+# Draw the heatmap
+sns.heatmap(corr, mask=mask, cmap=cmap, vmax=1.0, vmin=-1.0, center=0,
+            square=True, linewidths=.5, cbar_kws={"shrink": .5})
+plt.show()
+#
+# Store the upper triangle of the correlation matrix into an Excel sheet
+corrUpperTri = corr.where(mask)
+writer = pd.ExcelWriter('../data/EEG/EEG_Shifted_Correlation_Matrix_without_outliers.xlsx')
 corrUpperTri.to_excel(writer, 'CorrelationMatrix')
 writer.save()
 #
@@ -126,17 +173,8 @@ def plotBoxPlots(data, arrLabels, titleSuffix):
                            )
     plt.show()
 #
-# Removing Outliers
-outLierIndexes = set()
-upperLimit = 5000
-for x in range(shifted_df_X_count):
-    shifted_df_X = np.array(shifted_df_X)
-    outLiers = np.where(shifted_df_X[:, x] > upperLimit)[0]
-    if len(outLiers) > 0:
-        [(outLierIndexes.add(outLiers[i])) for i in range(len(outLiers))]
-#
-outLierIndexes = list(outLierIndexes)
-outLierIndexes.sort()
+# Plotting Box Plots
+outLierIndexes = get_outlier_indexes(shifted_df_X, shifted_df_X_count, 5000)
 print('Extreme outliers\nTotal:   ', len(outLierIndexes), \
     '\nIndexes: ', outLierIndexes)
 [(print(DataFrame(shifted_df_X).iloc[[outlier]])) for outlier in outLierIndexes]
@@ -161,13 +199,21 @@ plt.show()
 # Feature Selection using RandomForests
 # https://towardsdatascience.com/running-random-forests-inspect-the-feature-importances-with-this-code-2b00dd72b92e
 from sklearn.ensemble import RandomForestClassifier
-rf = RandomForestClassifier()
+rf = RandomForestClassifier(n_estimators=500)
 X_train = shifted_df_X
 y_train = shifted_df_y
-# print(X_train[-1])
-# print(y_train[-1])
 rf.fit(X_train, y_train)
 feature_importances = pd.DataFrame(rf.feature_importances_,
                                    index = shifted_columns,
                                     columns=['importance']).sort_values('importance', ascending=False)
+std = np.std([tree.feature_importances_ for tree in rf.estimators_], axis=0)
 print(feature_importances)
+# Plot the feature importances of the forest
+plt.figure()
+plt.title("Feature importance")
+objects = list(feature_importances.axes[0])
+y_pos = np.arange(len(objects))
+feature_importance = np.array(feature_importances)
+plt.bar(y_pos, feature_importance, align='center', alpha=0.5)
+plt.xticks(y_pos, objects)
+plt.show()
